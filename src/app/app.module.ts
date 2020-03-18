1d7148abf2f3e94bef4e24ad6fa1a1b39e8e87e1
Rejun Philip <rejun.philip@gmail.com>
2020-03-18 14:41:32 +0530

    02_01b Done

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { AppComponent } from './app.component';
+@NgModule({
+    imports: [
+        BrowserModule
+    ], //other angular modules you need
+    declarations: [
+        AppComponent
+    ], //to make compnents directives and pipes
+    bootstrap: [AppComponent]  // root module for knowing the entry point
+})
+export class AppModule {{
+
+}}
